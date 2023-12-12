7ffb9642828daf7c47d1c9beceaa5121230a16ed
baraa <hey.baraa@gmail.com>
2023-12-12 05:34:56 +0200

    feat: preview link

--- apps/home/src/app/(root)/app/(no-sidebar)/[teamUrl]/[projectUrl]/[pageId]/top-actions/index.tsx
+++ apps/home/src/app/(root)/app/(no-sidebar)/[teamUrl]/[projectUrl]/[pageId]/top-actions/index.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'next/navigation'
+import Link from 'next/link'
 import BackButton from './back-button'
 import ExportButton from './export'
 import ImportSite from './import-site'
@@ -10,20 +11,23 @@ const TopActions = () => {
   const { project } = useProject()
   const activePagePath = project?.pages.find((page) => page.id === pageId)?.path
 
+  const previewUrl = `${process.env.NEXT_PUBLIC_SITE_URL}/p/${
+    projectUrl as string
+  }${activePagePath || '/'}`
+
   return (
     <div className="relative z-50 flex items-center justify-between w-full h-12 px-5 border-b select-none bg-background border-border">
       <div className="flex items-center gap-2">
         <BackButton />
         <div className="flex items-center gap-4">
           <p className="text-sm font-medium text-white">{project?.name}</p>
-          <p className="px-2 rounded-md bg-accent">
-            <span className="text-xs font-normal text-white/70">
-              {projectUrl}.builder.baraa.app
-            </span>
-            <span className="text-sm font-medium text-white">
-              {activePagePath || '/'}
-            </span>
-          </p>
+          <Link
+            className="px-2 text-xs font-normal rounded-md bg-accent text-white/70"
+            href={previewUrl}
+            rel="noopener noreferrer"
+            target="_blank">
+            {previewUrl.replace(/https?:\/\//, '')}
+          </Link>
         </div>
       </div>
       <div className="flex flex-row-reverse items-center gap-4">
