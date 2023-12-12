6a2d6bff1e3513d6e7167daa45caa13150ae33d6
baraa <hey.baraa@gmail.com>
2023-12-12 05:41:53 +0200

    fix: fix issue with pages not being relative to the user project

--- apps/server/src/controllers/team/get-project-pages.controller.ts
+++ apps/server/src/controllers/team/get-project-pages.controller.ts
@@ -2,17 +2,30 @@ import { Request, Response } from 'express'
 
 const getProjectPagesController = async (req: Request, res: Response) => {
   try {
-    const { projectId } = req.params
+    const { projectUrl } = req.params
     const { prisma, user } = req.context
+
+    const project = await prisma.project.findUnique({
+      where: {
+        url: projectUrl,
+      },
+      select: {
+        id: true,
+      },
+    })
+
+    if (!project) {
+      return res.status(404).json({ message: 'Project not found' })
+    }
 
     const pages = await prisma.page.findMany({
       where: {
-        projectId
+        projectId: project.id,
       },
       select: {
         id: true,
         path: true,
-      }
+      },
     })
 
     // group pages with depth based on url segments
